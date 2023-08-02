74ced18f6fe6f6de5b23a17152b591d1b9b75092
Reinaldo Romero <reinaldoromero696@gmail.com>
2023-08-02 19:39:33 -0400

    class8 generator index | async-course

---
+++ src/generator/index.js
@@ -0,0 +1,24 @@
+//generator
+
+function* gen(){
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const g = gen();
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+
+function* iterate (array) {
+    for(let value of array) {
+        yield value
+    }
+}
+
+const it = iterate(['oscar', 'lucia', 'juan', 'omar', 'ana']);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
