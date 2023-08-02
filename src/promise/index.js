68883658b31e6bcb68f99a47bb0f60d00ccae72c
Reinaldo Romero <reinaldoromero696@gmail.com>
2023-08-02 13:01:52 -0400

    class3 promise | async-course

---
+++ src/promise/index.js
@@ -0,0 +1,21 @@
+//promesas
+
+const promise = new Promise(function (resolve, reject) {
+    resolve('hey')
+});
+
+const cows = 9;
+
+const countCows = new Promise(function (resolve, reject) {
+   if (cows > 10) {
+    resolve(`we have ${cows} cows on the farm`);
+   } else {
+    reject('there is no cows on the farm');
+   }
+});
+
+countCows.then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+}).finally(() => console.log('finally'));
